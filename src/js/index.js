7726d7a2599029280cfa10169938c67fc6ea5eb4
leofrigeri <123018960+leofrigeri@users.noreply.github.com>
2023-01-22 19:45:07 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,19 @@
+const botaoTrailer = document.querySelector(".botao-trailer");
+const botaoFecharModal = document.querySelector(".fechar-modal");
+const video = document.getElementById("video");
+const modal = document.querySelector(".modal");
+const linkDoVideo = "https://www.youtube.com/embed/Cb4WV4aXBpk";
+
+function alternarModal(){
+    modal.classList.toggle("aberto");
+};
+
+botaoTrailer.addEventListener("click", () => {
+    alternarModal();
+    video.setAttribute("src", "https://www.youtube.com/embed/Cb4WV4aXBpk");
+});
+
+botaoFecharModal.addEventListener("click", () => {
+    alternarModal();
+    video.setAttribute("src", "");
+});
